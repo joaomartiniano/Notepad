cfc7e66c0efb9848dfdd9b546a505179a24ce3ed
João Martiniano <joao.martiniano@gmail.com>
2019-07-15 18:04:37 +0100

    Alteração de propriedades da caixa de texto Texto

--- Notepad/Form1.Designer.cs
+++ Notepad/Form1.Designer.cs
@@ -39,7 +39,7 @@
             this.MenuFormSair = new System.Windows.Forms.ToolStripMenuItem();
             this.ajudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.MenuFormAcerca = new System.Windows.Forms.ToolStripMenuItem();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Texto = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -70,36 +70,36 @@
             // MenuFormNovo
             // 
             this.MenuFormNovo.Name = "MenuFormNovo";
-            this.MenuFormNovo.Size = new System.Drawing.Size(180, 22);
+            this.MenuFormNovo.Size = new System.Drawing.Size(161, 22);
             this.MenuFormNovo.Text = "&Novo";
             // 
             // MenuFormAbrir
             // 
             this.MenuFormAbrir.Name = "MenuFormAbrir";
-            this.MenuFormAbrir.Size = new System.Drawing.Size(180, 22);
+            this.MenuFormAbrir.Size = new System.Drawing.Size(161, 22);
             this.MenuFormAbrir.Text = "&Abrir...";
             // 
             // MenuFormGuardar
             // 
             this.MenuFormGuardar.Name = "MenuFormGuardar";
-            this.MenuFormGuardar.Size = new System.Drawing.Size(180, 22);
+            this.MenuFormGuardar.Size = new System.Drawing.Size(161, 22);
             this.MenuFormGuardar.Text = "&Guardar";
             // 
             // MenuFormGuardarComo
             // 
             this.MenuFormGuardarComo.Name = "MenuFormGuardarComo";
-            this.MenuFormGuardarComo.Size = new System.Drawing.Size(180, 22);
+            this.MenuFormGuardarComo.Size = new System.Drawing.Size(161, 22);
             this.MenuFormGuardarComo.Text = "Guardar &Como...";
             // 
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(158, 6);
             // 
             // MenuFormSair
             // 
             this.MenuFormSair.Name = "MenuFormSair";
-            this.MenuFormSair.Size = new System.Drawing.Size(180, 22);
+            this.MenuFormSair.Size = new System.Drawing.Size(161, 22);
             this.MenuFormSair.Text = "&Sair";
             // 
             // ajudaToolStripMenuItem
@@ -113,22 +113,29 @@
             // MenuFormAcerca
             // 
             this.MenuFormAcerca.Name = "MenuFormAcerca";
-            this.MenuFormAcerca.Size = new System.Drawing.Size(180, 22);
+            this.MenuFormAcerca.Size = new System.Drawing.Size(176, 22);
             this.MenuFormAcerca.Text = "&Acerca do Notepad";
             // 
-            // textBox1
+            // Texto
             // 
-            this.textBox1.Location = new System.Drawing.Point(289, 99);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 1;
+            this.Texto.AcceptsTab = true;
+            this.Texto.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Texto.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Texto.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Texto.Location = new System.Drawing.Point(0, 24);
+            this.Texto.MaxLength = 65535;
+            this.Texto.Multiline = true;
+            this.Texto.Name = "Texto";
+            this.Texto.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.Texto.Size = new System.Drawing.Size(584, 187);
+            this.Texto.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 211);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Texto);
             this.Controls.Add(this.menuStrip1);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -155,7 +162,7 @@
         private System.Windows.Forms.ToolStripMenuItem MenuFormSair;
         private System.Windows.Forms.ToolStripMenuItem ajudaToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem MenuFormAcerca;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox Texto;
     }
 }
 

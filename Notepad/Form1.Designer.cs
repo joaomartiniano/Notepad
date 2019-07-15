aeab94d58c9abb470c7425455b8e06965bac6750
João Martiniano <joao.martiniano@gmail.com>
2019-07-15 16:01:02 +0100

    Inserção do menu e respetivos itens

--- Notepad/Form1.Designer.cs
+++ Notepad/Form1.Designer.cs
@@ -29,23 +29,123 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ficheiroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuFormNovo = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuFormAbrir = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuFormGuardar = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuFormGuardarComo = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.MenuFormSair = new System.Windows.Forms.ToolStripMenuItem();
+            this.ajudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuFormAcerca = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ficheiroToolStripMenuItem,
+            this.ajudaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(584, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ficheiroToolStripMenuItem
+            // 
+            this.ficheiroToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuFormNovo,
+            this.MenuFormAbrir,
+            this.MenuFormGuardar,
+            this.MenuFormGuardarComo,
+            this.toolStripMenuItem1,
+            this.MenuFormSair});
+            this.ficheiroToolStripMenuItem.Name = "ficheiroToolStripMenuItem";
+            this.ficheiroToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.ficheiroToolStripMenuItem.Text = "&Ficheiro";
+            // 
+            // MenuFormNovo
+            // 
+            this.MenuFormNovo.Name = "MenuFormNovo";
+            this.MenuFormNovo.Size = new System.Drawing.Size(180, 22);
+            this.MenuFormNovo.Text = "&Novo";
+            // 
+            // MenuFormAbrir
+            // 
+            this.MenuFormAbrir.Name = "MenuFormAbrir";
+            this.MenuFormAbrir.Size = new System.Drawing.Size(180, 22);
+            this.MenuFormAbrir.Text = "&Abrir...";
+            // 
+            // MenuFormGuardar
+            // 
+            this.MenuFormGuardar.Name = "MenuFormGuardar";
+            this.MenuFormGuardar.Size = new System.Drawing.Size(180, 22);
+            this.MenuFormGuardar.Text = "&Guardar";
+            // 
+            // MenuFormGuardarComo
+            // 
+            this.MenuFormGuardarComo.Name = "MenuFormGuardarComo";
+            this.MenuFormGuardarComo.Size = new System.Drawing.Size(180, 22);
+            this.MenuFormGuardarComo.Text = "Guardar &Como...";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // MenuFormSair
+            // 
+            this.MenuFormSair.Name = "MenuFormSair";
+            this.MenuFormSair.Size = new System.Drawing.Size(180, 22);
+            this.MenuFormSair.Text = "&Sair";
+            // 
+            // ajudaToolStripMenuItem
+            // 
+            this.ajudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuFormAcerca});
+            this.ajudaToolStripMenuItem.Name = "ajudaToolStripMenuItem";
+            this.ajudaToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.ajudaToolStripMenuItem.Text = "&Ajuda";
+            // 
+            // MenuFormAcerca
+            // 
+            this.MenuFormAcerca.Name = "MenuFormAcerca";
+            this.MenuFormAcerca.Size = new System.Drawing.Size(180, 22);
+            this.MenuFormAcerca.Text = "&Acerca do Notepad";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 211);
+            this.Controls.Add(this.menuStrip1);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ficheiroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MenuFormNovo;
+        private System.Windows.Forms.ToolStripMenuItem MenuFormAbrir;
+        private System.Windows.Forms.ToolStripMenuItem MenuFormGuardar;
+        private System.Windows.Forms.ToolStripMenuItem MenuFormGuardarComo;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem MenuFormSair;
+        private System.Windows.Forms.ToolStripMenuItem ajudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MenuFormAcerca;
     }
 }
 

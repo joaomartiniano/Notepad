4ff862307a2a8cc64195683e5bc411dbaeb098ed
João Martiniano <joao.martiniano@gmail.com>
2019-07-15 18:07:28 +0100

    Importar System.IO

--- Notepad/Form1.Designer.cs
+++ Notepad/Form1.Designer.cs
@@ -143,6 +143,7 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
--- Notepad/Form1.cs
+++ Notepad/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Notepad
 {
@@ -14,6 +15,11 @@ namespace Notepad
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }

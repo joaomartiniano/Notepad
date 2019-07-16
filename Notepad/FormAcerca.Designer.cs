7f61c2cdb2d0c8d918dcff95b7670e841cff250f
João Martiniano <joao.martiniano@gmail.com>
2019-07-16 10:18:19 +0100

    Editar propriedades do botão

--- Notepad/FormAcerca.Designer.cs
+++ Notepad/FormAcerca.Designer.cs
@@ -33,7 +33,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.BtFechar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -87,21 +87,21 @@
             this.label4.TabIndex = 4;
             this.label4.Text = "Desenvolvido por João Martiniano";
             // 
-            // button1
+            // BtFechar
             // 
-            this.button1.Location = new System.Drawing.Point(391, 137);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.BtFechar.Location = new System.Drawing.Point(391, 137);
+            this.BtFechar.Name = "BtFechar";
+            this.BtFechar.Size = new System.Drawing.Size(75, 23);
+            this.BtFechar.TabIndex = 5;
+            this.BtFechar.Text = "Fechar";
+            this.BtFechar.UseVisualStyleBackColor = true;
             // 
             // FormAcerca
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(478, 172);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BtFechar);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -128,6 +128,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BtFechar;
     }
 }

7d92ab81cbb48e2b9268377fab3b1048f999f12b
João Martiniano <joao.martiniano@gmail.com>
2019-07-16 09:33:35 +0100

    Editar propriedades do label "Notepad"

--- Notepad/FormAcerca.Designer.cs
+++ Notepad/FormAcerca.Designer.cs
@@ -46,11 +46,13 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(339, 72);
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label1.Location = new System.Drawing.Point(97, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.Size = new System.Drawing.Size(124, 37);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Notepad";
             // 
             // FormAcerca
             // 
